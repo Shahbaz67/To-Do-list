bfd564c1f6253f418851e0fb7c8c08d55284f6b9
Shahbaz <mdshahbaznsr5@gmail.com>
2021-05-20 19:57:05 +0530

    first commit

---
+++ MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>ldvfv jhdgufvh fdjdvfvsjfnb nvjvdvf
+            </p>
+        </div>
+    )
+}
